5a49f15039185a1a8dbf0743ab01d67231087295
Musa <musamaxwell15@gmail.com>
2023-04-14 17:52:10 +0300

    Payment Backend

---
+++ public/paymentModel.js
@@ -0,0 +1,28 @@
+const  mongoose  = require("mongoose");
+const { Schema } = mongoose;
+
+
+
+const paymentSchema = new Schema({
+
+    number: {
+        type: String,
+        required: true,
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+    trnx_id: {
+        type: String,
+        required: true,
+    },
+   timestamp: {
+
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Payment = mongoose.model("Payment", paymentSchema);
+module.exports = Payment;
--- script.js
+++ script.js
@@ -1,7 +1,44 @@
 const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
 const app = express();
 const port = 3000;
 const path = require('path');
+
+app.set('view engine', 'ejs');
+
+const Payment = require('./public/paymentModel');
+
+
+mongoose.connect('mongodb+srv://Admin:admin123@cluster0.pghdyh2.mongodb.net/?retryWrites=true&w=majority').then(() => {
+    console.log("Connected to MongoDB");
+}).catch((error) => {
+    console.log(error.message);
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+async function getPayment(){
+    const Payments = await Payment.find({});
+    return Payments;
+    
+}
+
+app.get('/payments', (req, res) => {
+
+    getPayment().then(function (payments){
+        res.render('index',{
+            paymentList: payments
+        })
+    })
+
+
+});
 
 app.use(express.static(__dirname));
 
@@ -15,6 +52,110 @@ app.get('/', (req, res) => {
    
 
 });
+
+const generateToken = async( req, res, next) => {
+    const secret = "LcjDnANjEMykLge5";
+    const consumer = "CA9HkTEzuH1HXuZzHmHs7AGyDIercU0q";
+    const auth = new Buffer.from(`${consumer}:${secret}`).toString('base64');
+
+    await axios.get("https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials", {
+        headers: {
+            authorization: `Basic ${auth}`,
+        }
+    }).then((data) => {
+        token = data.data.access_token;
+        console.log(data.data);
+        next();
+    }).catch((error) => {
+        console.log(error);
+        //res.status(400).json(error.message);
+    })
+
+
+
+} 
+
+app.post('/stkPush', generateToken,  async( req, res) => {
+    const phone = req.body.phone.substring(1);
+    const amount = req.body.amount;
+    const date = new Date();
+    const timestamp = 
+            date.getFullYear() +
+            ("0" + (date.getMonth() + 1)).slice(-2) +
+                ("0" + date.getDate()).slice(-2) +
+                ("0" + date.getHours()).slice(-2) +
+                ("0" + date.getMinutes()).slice(-2) +
+                ("0" + date.getSeconds()).slice(-2);
+    const shortCode = "174379";
+    const passKey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
+    const password = new Buffer.from(shortCode + passKey + timestamp).toString('base64');
+         
+        await axios.post(
+           " https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
+            {
+                    
+                BusinessShortCode: shortCode,    
+                Password: password,    
+                Timestamp: timestamp,    
+                TransactionType: "CustomerPayBillOnline",    
+                Amount: amount,   
+                PartyA: `254${phone}`,    
+                PartyB: shortCode,    
+                PhoneNumber: `254${phone}`,    
+                CallBackURL:"https://silent-mouse-67.telebit.io/callback",    
+                AccountReference:"Test",    
+                TransactionDesc:"Test"
+            },
+            {
+                headers: {
+                    authorization: `Bearer ${token}`,
+                },
+            }
+        ).then((data) => {
+            console.log(data.data);
+            res.status(200).json(data.data);
+
+        }).catch((error) => {
+            console.log(error);
+            res.status(400).json(error.message);
+        });
+});
+
+app.post("/callback", (req, res) => {
+    const callbackData = req.body;
+    console.log(callbackData.Body);
+    if(!callbackData.Body.stkCallback.CallbackMetadata){
+        console.log(callbackData.Body);
+        return res.json("ok");      
+    }
+
+    console.log(callbackData.Body.stkCallback.CallbackMetadata);
+    const phone = callbackData.Body.stkCallback.CallbackMetadata.Item[4].Value;
+    const amount = callbackData.Body.stkCallback.CallbackMetadata.Item[0].Value;
+    const trnx_id = callbackData.Body.stkCallback.CallbackMetadata.Item[1].Value; 
+    const timestamp = callbackData.Body.stkCallback.CallbackMetadata.Item[3].Value;
+
+    console.log({phone, amount, trnx_id, timestamp});
+
+    const payment = new Payment({
+        number: phone,
+        amount: amount,
+        trnx_id: trnx_id,        
+    });
+
+    payment.save().then((data) => {
+        console.log(data);
+        res.status(200).json({message: "Payment successful", data});
+    }
+    ).catch((error) => {
+        console.log(error);
+        res.status(400).json(error.message);
+    }
+    );
+
+})
+
+
 
 
 app.listen(port, () => {
